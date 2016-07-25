0a82261e7bb2eb06d508c6ca0f4b1d1fb1975fcc
Thaina Yu <thaina_greenwolf@hotmail.com>
2016-07-25 11:13:12 +0700

    redirect in hierachy

--- main.js
+++ main.js
@@ -2,12 +2,35 @@
 
 require.config({ paths: { 'vs': '/node_modules/monaco-editor/min/vs' } });
 var PromiseMonaco = new Promise((resolve) => {
+	if(monaco)
+		return resolve();
+
 	require(['vs/editor/editor.main'], () => resolve());
 });
 
+/** @type {monaco.editor.IStandaloneCodeEditor} */
+var editor;
+/** @type {monaco.editor.IStandaloneCodeEditor} */
+var hierachy;
 
 /* exported */
 function Body($scope) {
+	var redirect	= sessionStorage.redirect;
+	if(redirect)
+	{
+		history.pushState(null,null,redirect);
+		delete sessionStorage.redirect;
+	}
+
+	PromiseMonaco.then(() => {
+		if (!hierachy) {
+			hierachy = monaco.editor.create(document.getElementById('container'), { language: 'json',wrappingColumn:-1 });
+			hierachy.getModel().detectIndentation(false,4);
+		}
+	
+		hierachy.setValue(JSON.stringify(redirect,null,"\t"));
+	});
+
 	function Cloudant(method, path) {
 		return $.get({
 			method: method,
@@ -16,14 +39,11 @@ function Body($scope) {
 		});
 	}
 
-	/** @type {monaco.editor.IStandaloneCodeEditor} */
-	var editor;
 	$scope.login = function ($event) {
 		$event.preventDefault();
 		Cloudant("GET", "/_users/_all_docs?startkey=\"_design/\"&endkey=\"_design/\uFFFF\"").then((data, status) => {
 			$scope.status = status;
 			$scope.$apply();
-
 			return data;
 		}, (req, status, err) => {
 			console.error(err);
@@ -33,7 +53,7 @@ function Body($scope) {
 			return PromiseMonaco.then(() => data);
 		}, (err) => console.error(err)).then((data) => {
 			if (!editor) {
-				editor = monaco.editor.create(document.getElementById('container'), { language: 'json' });
+				editor = monaco.editor.create(document.getElementById('container'), { language: 'json',wrappingColumn:-1 });
 				editor.getModel().detectIndentation(false,4);
 			}
 
